a4421533f3c219d33b3b1289c6cec9b10a87afb7
ydkulks <ydkulks2@gmail.com>
2020-11-25 11:09:03 +0530

    Removed

--- .mycommand.sh
+++ .mycommand.sh
@@ -61,20 +61,6 @@ function webproject(){
     #vim index.html
 }
 
-#For new projects
-function newproject(){
-    #cd
-    #echo 'Creating new project: ' $1
-    #python3 create.py $1 
-    #cd /mnt/e/$1
-    #echo '# README'>README.md
-    #git init
-    #git remote add origin https://github.com/ydkulks/$1.git 
-    #git add .
-    #git commit -m "Initial commit"
-    #git push -u origin master 
-    #echo 'Created new project in /mnt/e/'
-}
 
 #For printing the process of shell automation in terminal
 function auto_process(){
