62a9e95cc3fcfc433acebe459019177fd0b2d46d
ydkulks <ydkulks2@gmail.com>
2024-01-18 16:42:51 +0530

    Visual selection color and colorscheam
    Changed selection color to blue and black colors. And, colorscheam to
    onedark

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,14 +1,14 @@
 local vim = vim
 function ColorMyPencils(color)
-	color = "palenight"
-	-- color = "onedark"
+	-- color = "palenight"
+	color = "onedark"
 	vim.cmd.colorscheme(color)
 	vim.api.nvim_set_hl(0,"Normal", {bg = "none"})
 	vim.api.nvim_set_hl(0,"NormalFloat", {bg = "none"})
   vim.api.nvim_set_hl(0,"Visual", { ctermbg = "blue", ctermfg = "black"})
-  -- vim.api.nvim_exec([[
-  -- hi Visual cterm=bold gui=bold
-  -- ]],false)
+  vim.api.nvim_exec([[
+    hi Visual cterm=bold guibg=#61afef guifg=#282c34
+  ]],false)
 end
 
 ColorMyPencils()
