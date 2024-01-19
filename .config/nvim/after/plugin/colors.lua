1820d7ffdfce5a094cfdec2e50c0a4b1314fbfeb
ydkulks <ydkulks2@gmail.com>
2024-01-19 16:54:47 +0530

    Visual selection config
    Configured visual selection config only for palenight colorscheme

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,14 +1,17 @@
 local vim = vim
 function ColorMyPencils(color)
-	-- color = "palenight"
-	color = "onedark"
+	color = "palenight"
+	-- color = "onedark"
+
+  if color == "palenight" then
+    vim.opt.termguicolors = false
+    vim.api.nvim_set_hl(0,"Visual", { ctermbg=238, italic=false, bold=true })
+  end
+
 	vim.cmd.colorscheme(color)
+
 	vim.api.nvim_set_hl(0,"Normal", {bg = "none"})
 	vim.api.nvim_set_hl(0,"NormalFloat", {bg = "none"})
-  vim.api.nvim_set_hl(0,"Visual", { ctermbg = "blue", ctermfg = "black"})
-  vim.api.nvim_exec([[
-    hi Visual cterm=bold guibg=#61afef guifg=#282c34
-  ]],false)
 end
 
 ColorMyPencils()
