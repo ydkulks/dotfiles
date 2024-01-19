3f54c0834d767e9a3c425903e5f6e6b9ae1268ba
ydkulks <ydkulks2@gmail.com>
2024-01-19 16:57:00 +0530

    Cleaned unused config
    Removed coc.lua and prettier.lua

--- .config/nvim/after/plugin/prettier.lua
+++
@@ -1,7 +0,0 @@
--- local lspconfig = require('lspconfig')
-
--- lspconfig.prettier.setup{
--- 	cmd = {"~/.config/nvim/"}
--- }
--- vim.cmd('let g:prettier#autoformat=1')
--- vim.cmd('let g:prettier#autoformat_require_pragma=0')
