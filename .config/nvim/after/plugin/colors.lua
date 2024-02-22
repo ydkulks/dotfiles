299f150791e72853789da503d92bfb19a2775ce4
ydkulks <ydkulks2@gmail.com>
2024-02-22 17:39:47 +0530

    Gruvbox, netrw and pandoc_template

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,18 +1,23 @@
 local vim = vim
 function ColorMyPencils(color)
-  color = "palenight"
-  -- color = "gruvbox"
-
-  if color == "palenight" then
-    vim.opt.termguicolors = false
-    -- vim.api.nvim_set_hl(0,"Visual", { ctermbg=238, italic=false, bold=true })
-  end
+  vim.opt.termguicolors = false
 
   vim.cmd.colorscheme(color)
 
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-  vim.api.nvim_set_hl(0, "Visual", { ctermbg = 238, italic = false, bold = true })
+
+  if color == "palenight" then
+    vim.api.nvim_set_hl(0, "Visual", { ctermbg = 238, italic = false, bold = true })
+  elseif color == "gruvbox" then
+    vim.api.nvim_set_hl(0, "SignColumn", { ctermbg = "none" })
+    -- vim.api.nvim_set_hl(0, "GitGutterAdd", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "GitGutterChange", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "GitGutterDelete", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Visual", { bg = "#3c3836", italic = false, bold = true })
+  end
+
 end
 
-ColorMyPencils()
+ColorMyPencils("palenight")
+-- ColorMyPencils("gruvbox")
