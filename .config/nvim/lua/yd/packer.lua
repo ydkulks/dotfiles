c2f924fe92773575c563bf0dcbaaf7256510129a
ydkulks <ydkulks2@gmail.com>
2024-12-02 22:28:21 +0530

    Markdown render
    Render markdown in neovim

--- .config/nvim/lua/yd/packer.lua
+++ .config/nvim/lua/yd/packer.lua
@@ -39,11 +39,21 @@ return require('packer').startup({ function(use)
     end
   }
   -- use 'ThePrimeagen/vim-be-good'
+  use {
+    'MeanderingProgrammer/render-markdown.nvim',
+    -- after = { 'nvim-treesitter' },
+    -- requires = { 'echasnovski/mini.nvim', opt = true }, -- if you use the mini.nvim suite
+    -- requires = { 'echasnovski/mini.icons', opt = true }, -- if you use standalone mini plugins
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }, -- if you prefer nvim-web-devicons
+    config = function()
+      require('render-markdown').setup({})
+    end,
+  }
 
   -- My plugins
   -- use 'ydkulks/speedster.nvim'
   -- use '/home/yd/Projects/luaPlugins/conceal.nvim'
-  use '/home/yd/Projects/luaPlugins/AST.nvim'
+  -- use '/home/yd/Projects/luaPlugins/AST.nvim'
 
   -- use {
   --   "ydkulks/AST.nvim",
