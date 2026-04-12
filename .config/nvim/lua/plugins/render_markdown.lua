afacb19a91b9ff04e5f4be28657b8c2960e07b9c
ydkulks <ydkulks2@gmail.com>
2026-04-12 12:00:16 +0530

    feat: Yazi config
    Added yazi config with githead plugin

--- .config/nvim/lua/plugins/render_markdown.lua
+++ .config/nvim/lua/plugins/render_markdown.lua
@@ -5,7 +5,6 @@ return {
     event = "VeryLazy",
     after = { 'nvim-treesitter' },
     -- dependencies = { 'echasnovski/mini.icons', opt = true }, -- if you use standalone mini plugins
-    dependencies = { 'nvim-tree/nvim-web-devicons', opt = true },   -- if you use standalone mini plugins
-    opts = {},
+    dependencies = { 'nvim-tree/nvim-web-devicons', opt = true }, -- if you use standalone mini plugins
   },
 }
